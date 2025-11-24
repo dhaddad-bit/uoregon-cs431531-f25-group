ca243d3dc94b773210be1bf1645088e811ef1024
anorris26 <alnorris113@gmail.com>
2025-11-24 10:42:45 -0800

    added vertices

---
+++ anna_code/make_b.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char *name_vector_file(char *name) {
+    
+}
+
+// for simplicity's sake, this only generates doubles between 0 and 1
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s {number of rows} {name}\n", argv[0]);
+    }
+    
+    int rows = atoi(argv[1]);
+    srand((unsigned)time(NULL));
+
+    FILE *fptr;
+    char *name = argv[2];
+    
+    char *result = malloc(strlen("b_") + strlen(name) + strlen(".mtx") + 1);
+
+    strcpy(result, "b_");
+    strcat(result, name);
+    strcat(result, ".mtx");
+
+    fptr = fopen(result, "w");
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    
+
+    fprintf(fptr, "%d\n", rows);
+
+    for (int i = 0; i < rows; i++) {
+        double rand_num = (double)rand() / (double)RAND_MAX;
+        fprintf(fptr, "%.8f\n", rand_num);
+    }
+
+    fclose(fptr);
+
+    free(result);
+
+    return 0;
+}
